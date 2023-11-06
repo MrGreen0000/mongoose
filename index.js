8095a64565834631a6b41f97b5a586da9d50fe0f
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-11-06 16:06:02 +0100

    update

--- index.js
+++ index.js
@@ -14,10 +14,22 @@ mongoose
   .connect("mongodb://naji:naji1410@127.0.0.1:27017/dyma?authSource=admin", {})
   .then(() => {
     console.log("Connected");
-    return Chapters.find({}).limit(2).exec();
+
+    const newChapters = new Chapters({
+      title: "flutter",
+      nbrOfLesson: 40,
+      index: 4,
+      active: true,
+    });
+
+    return newChapters.save();
   })
   .then((documents) => {
-    console.log(documents);
+    console.log("Inserted docment:", documents);
+    return Chapters.find({}).exec();
+  })
+  .then((documents) => {
+    console.log("All documents:", documents);
   })
   .catch((err) => {
     console.log(err);
