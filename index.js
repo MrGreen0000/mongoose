071e2eb8b18ae4b4aaf8009da4b04e6ace07c4b0
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-12-22 18:07:29 +0100

    update

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const chapterSchema = schema({
-  title: String,
-  nbrOfLesson: Number,
-  index: Number,
-  active: Boolean,
-});
+const chapterSchema = schema(
+  {
+    title: String,
+    nbrOfLesson: Number,
+    index: Number,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Chapters = mongoose.model("chapters", chapterSchema);
 
 mongoose
-  .connect("mongodb://naji:naji1410@127.0.0.1:27017/dyma?authSource=admin", {})
+  .connect("mongodb+srv://naji:naji1410@cluster0.2mlqv2t.mongodb.net/", {})
   .then(() => {
     console.log("Connected");
 
-    const newChapters = new Chapters({
-      title: "flutter",
-      nbrOfLesson: 40,
-      index: 4,
+    const newChapter = new Chapters({
+      title: "react",
+      nbrOfLesson: 15,
       active: true,
     });
 
-    return newChapters.save();
-  })
-  .then((documents) => {
-    console.log("Inserted docment:", documents);
-    return Chapters.find({}).exec();
-  })
-  .then((documents) => {
-    console.log("All documents:", documents);
+    newChapter.save();
   })
   .catch((err) => {
     console.log(err);
